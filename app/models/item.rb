bc8e53dda846848b3bad352fb13cb0ff527b6b4c
kako05 <banana_manbou@yahoo.co.jp>
2024-05-08 14:05:45 +0900

    出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping, :region, :schedule_date
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :region
+  belongs_to :schedule_date
 
   with_options presence: true do
-    validates :title, :text, :price, :user,
-    validates :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :title, :text, :price, :user
+    validates :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, numericality: { other_than: 1 }
     validates :price, format: { with: /\A\d+\z/, message: "half-width numbers only" }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  end
 end
