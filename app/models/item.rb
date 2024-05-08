452559cba52c9b68d57839116d9be3fa9ab930a8
kako05 <banana_manbou@yahoo.co.jp>
2024-05-08 10:55:26 +0900

    画像加工ツールとActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :shipping, :region, :schedule_date
+  has_one_attached :image
 
   validates :title, :text, :price, :user, presence: true
   validates :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, numericality: { other_than: 1, message: "can't be blank" }
