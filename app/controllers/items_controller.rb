0710a51f05ae269ba52a8a031b19c99495e8a53c
kako05 <banana_manbou@yahoo.co.jp>
2024-05-08 14:49:55 +0900

    出品商品を保存する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_paramus)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_paramus
+    params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, :price_id, :fee, :profit).merge(user_id: current_user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :password, format: {with: /\A(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]+\z/, message: "is invalid. Please input a combination of half-width alphanumeric characters."}
   with_options presence: true do
     validates :nickname, :birthday
