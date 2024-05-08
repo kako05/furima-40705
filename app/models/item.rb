d557c2cfb2a32e42735a8d48cc1fa935c762e862
kako05 <banana_manbou@yahoo.co.jp>
2024-05-08 12:01:07 +0900

    Itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,12 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping, :region, :schedule_date
+  belongs_to :user
   has_one_attached :image
 
-  validates :title, :text, :price, :user, presence: true
-  validates :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, numericality: { other_than: 1, message: "can't be blank" }
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :shipping, :region, :schedule_date
+
+  with_options presence: true do
+    validates :title, :text, :price, :user,
+    validates :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :price, format: { with: /\A\d+\z/, message: "half-width numbers only" }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
 end
