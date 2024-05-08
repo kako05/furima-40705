bd0af338e629f6933cca05481d29ad18d0682721
kako05 <banana_manbou@yahoo.co.jp>
2024-05-08 20:44:04 +0900

    未ログインでログインページへ遷移/出品完了後トップページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :move_to_index, only: [:new]
   def index
   end
 
@@ -9,9 +9,8 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_paramus)
-    binding.pry
     if @item.save
-      redirect_to items_path
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -20,5 +19,11 @@ class ItemsController < ApplicationController
   private
   def item_paramus
     params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
