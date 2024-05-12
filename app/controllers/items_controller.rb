51a4abe26a5b0a78822e31f299a7deb20858c76e
kako05 <banana_manbou@yahoo.co.jp>
2024-05-12 21:17:30 +0900

    rubocopによる自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -18,13 +18,15 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_paramus
-    params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :shipping_id, :region_id, :schedule_date_id,
+                                 :price).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    return if user_signed_in?
+
+    redirect_to new_user_session_path
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname              {Faker::Name.unique.name}
-    email                 {Faker::Internet.email}
-    password              {"1a" + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    family_name           {person.last}
-    first_name            {person.first}
-    family_reading        {person.last.katakana}
-    first_reading         {person.first.katakana}
-    birthday              {Faker::Date.birthday}
+    nickname              { Faker::Name.unique.name }
+    email                 { Faker::Internet.email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    family_name           { person.last }
+    first_name            { person.first }
+    family_reading        { person.last.katakana }
+    first_reading         { person.first.katakana }
+    birthday              { Faker::Date.birthday }
   end
 end
