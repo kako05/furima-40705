085b04b6a9f115ad9cc598d4a445ce3fb967ec41
kako05 <banana_manbou@yahoo.co.jp>
2024-05-15 21:55:07 +0900

    自分の出品商品の購入ページに行けずトップページへ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_top, only: [:index, :create]
 
   def index
     @orderaddress = OrderAddress.new
@@ -24,5 +25,11 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postcode, :region_id, :city, :house_num, :building, :phone, :order_id).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def move_to_top
+    if current_user == @item.user
+    redirect_to items_path
+    end
   end
 end
