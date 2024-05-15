27eaca54656960554c94c2fc64c10054964f6c07
kako05 <banana_manbou@yahoo.co.jp>
2024-05-15 18:14:31 +0900

    ordersコントローラー作成と編集

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @orderaddress = OrderAddress.new
+  end
+
+  def create
+      @orderaddress = OrderAddress.new(order_params)
+    if @orderaddress.valid?
+      @orderaddress.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :region_id, :city, :house_num, :building, :phone, :order_id).merge(user_id: current_user.id, item_id: @item.id)
+  end
+end
