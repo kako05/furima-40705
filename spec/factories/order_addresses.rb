0c2a71561d0b6d4dd5ea230d218f1fdf9f4b1bc1
kako05 <banana_manbou@yahoo.co.jp>
2024-05-15 18:19:53 +0900

    ファクトリーボット生成の定義

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    postcode  { Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4)}
+    region_id { Faker::Number.between(from: 1, to: 47) }
+    city      { Faker::Address.city }
+    house_num { Faker::Address.street_address }
+    phone     { Faker::Number.leading_zero_number(digits: 11) }
+  
+    association :user
+    association :item
+  end
+end
