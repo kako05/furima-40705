cda8c6d43f717deb2b965df944d43132bc7104b6
kako05 <banana_manbou@yahoo.co.jp>
2024-05-16 22:00:29 +0900

    トークンのバリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :region_id, :city, :house_num, :building, :phone
+  attr_accessor :user_id, :item_id, :postcode, :region_id, :city, :house_num, :building, :phone, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_num, :phone, :postcode
+    validates :user_id, :item_id, :city, :house_num, :phone, :postcode, :token
   end
   validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: "input correctly" }
   validates :region_id, numericality: { other_than: 0, message: "Select" }
